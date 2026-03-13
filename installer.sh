70d065c7ede6f99a47db03a390985a03d375bbbb
Andre Brener <brener.andre@gmail.com>
2026-03-13 09:26:58 -0300

    update

--- installer.sh
+++ installer.sh
@@ -1,26 +1,38 @@
+# Cask apps
 brew install --cask arc
-brew install --cask spotify 
-brew install --cask google-chrome 
-brew install --cask whatsapp 
-brew install --cask discord 
-brew install --cask slack 
-brew install --cask visual-studio-code 
-brew install --cask telegram 
-brew install --cask hammerspoon 
 brew install --cask caffeine
+brew install --cask claude
+brew install --cask discord
+brew install --cask google-chrome
+brew install --cask hammerspoon
+brew install --cask linear-linear
+brew install --cask lulu
 brew install --cask notion
 brew install --cask pgadmin4
+brew install --cask slack
+brew install --cask spotify
+brew install --cask telegram
+brew install --cask visual-studio-code
+brew install --cask whatsapp
 brew install --cask zoom
 brew install --cask postman
 
+# Formulae
+brew install awscli
+brew install coreutils
+brew install ffmpeg
+brew install gh
+brew install go
+brew install mole
+brew install node
+brew install nvm
+brew install pandoc
+brew install postgresql@14
+brew install pyenv
+brew install pyenv-virtualenv
 brew install python3
-brew install nvm
-brew install postgresql
-brew install gpg
-brew install nvm
-
-pip3 install pandas
-pip3 install matplotlib 
-pip3 install jupyterlab
-pip3 install notebook
+brew install r
+brew install uv
+brew install weasyprint
+brew install yarn
 
